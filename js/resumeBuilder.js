2828d1d23d63d422660c89a70a71f27812657172
Juan Manuel Sánchez <juanmanuel.sanchez@icloud.com>
2014-12-16 19:08:07 +0100

    Change the createMapMaker function lat and lon variables to render the map

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -262,8 +262,8 @@ var map= {
     function createMapMarker(placeData) {
 
    
-    var lat = placeData.geometry.location.k;  
-    var lon = placeData.geometry.location.B;  
+    var lat = placeData.geometry.location.lat();  
+    var lon = placeData.geometry.location.lng();  
     var name = placeData.formatted_address;   
     var bounds = window.mapBounds;            
 
